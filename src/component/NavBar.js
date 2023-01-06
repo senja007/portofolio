211d3fe6abddb469e262738b00b120f1d3680824
dioproject <gagalginjal9@gmail.com>
2023-01-06 14:43:59 +0700

    responsive navbar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,22 +1,37 @@
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Nav, Navbar, Container, Offcanvas } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" variant="light" expand="lg">
-      <Container>
-        <Navbar.Brand>Neon Code</Navbar.Brand>        
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#service">Service</Nav.Link>
-            <Nav.Link href="#portofolio">Portofolio</Nav.Link>
-            <Nav.Link href="#team">Team</Nav.Link>
-            <Nav.Link href="#contactus">Contact Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+    {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
+      <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">Neon Code</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Neon Code
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#service">Service</Nav.Link>
+                <Nav.Link href="#portofolio">Portofolio</Nav.Link>
+                <Nav.Link href="#team">Team</Nav.Link>
+                <Nav.Link href="#contactus">Contact Us</Nav.Link>                
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    ))}
+    </>
   );
 }
 
