22be2fec349b64872befa0fe99dcc9cc41a4bc3c
dioproject <gagalginjal9@gmail.com>
2023-01-06 14:55:31 +0700

    responsive navbar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,34 +3,34 @@ import { Nav, Navbar, Container, Offcanvas } from 'react-bootstrap';
 const NavBar = () => {
   return (
     <>
-    {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-      <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-        <Container fluid>
-          <Navbar.Brand href="#">Neon Code</Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-${expand}`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                Neon Code
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#service">Service</Nav.Link>
-                <Nav.Link href="#portofolio">Portofolio</Nav.Link>
-                <Nav.Link href="#team">Team</Nav.Link>
-                <Nav.Link href="#contactus">Contact Us</Nav.Link>                
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    ))}
+      {['xl'].map((expand) => (
+        <Navbar key={expand} bg="dark" variant="dark" expand={expand} className="mb-3">
+          <Container>
+            <Navbar.Brand>Neon Code</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Neon Code
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-1">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#service">Service</Nav.Link>
+                  <Nav.Link href="#portofolio">Portofolio</Nav.Link>
+                  <Nav.Link href="#team">Team</Nav.Link>
+                  <Nav.Link href="#contactus">Contact Us</Nav.Link>                
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </>
   );
 }
