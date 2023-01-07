0e87148766920058d2bf343ab7478c517fe1673d
karim064 <102268088+karim064@users.noreply.github.com>
2023-01-07 11:33:35 +0700

    service
    service udah

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./component/NavBar";
+import Service from "./component/Service"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Service />
     </div>
   );
 }
---
+++ src/component/Service.js
@@ -0,0 +1,34 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import web from '../assets/img/web-developer.jpg'
+import uiux from '../assets/img/uiux.jpg'
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function Service() {
+  return (
+    <CardGroup style={{ width: '30rem' }}>
+        <Card >
+        <Card.Img variant="top" src={web} />
+        <Card.Body>
+            <Card.Title>Web Developer </Card.Title>
+            <Card.Text>
+            Website taek a
+            </Card.Text>
+            <Button variant="primary">Go</Button>
+        </Card.Body>
+        </Card>
+        <Card>
+        <Card.Img variant="top" src={uiux} />
+        <Card.Body>
+            <Card.Title>UI/UX Desaigner</Card.Title>
+            <Card.Text>
+            Desaign karepmu wes
+            </Card.Text>
+            <Button variant="primary">Go</Button>
+        </Card.Body>
+        </Card>
+    </CardGroup>
+  );
+}
+
+export default Service;
