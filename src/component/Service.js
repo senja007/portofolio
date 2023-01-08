2dbfb852b795b2bf1ba4ab716814271a20299d50
karim064 <102268088+karim064@users.noreply.github.com>
2023-01-08 12:33:40 +0700

    update Service.js
    ASW

--- src/component/Service.js
+++ src/component/Service.js
@@ -1,14 +1,13 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import {Button, Card, CardGroup} from 'react-bootstrap';
 import web from '../assets/img/web-developer.jpg'
-import uiux from '../assets/img/uiux.jpg'
-import CardGroup from 'react-bootstrap/CardGroup';
+import uiux from '../assets/img/uiux.jpg';
 import '../App.css'
 
 function Service() {
   return (
     <div>
-      <h1>Jasa Service</h1>
+      <h1>Layanan Kami</h1>
+      <h6>services we provide include</h6>
       <div className='list-service'>
         <CardGroup style={{ width: '30rem' }}>
             <Card >
