70f2c04ebdce8c83da56432878de70d705882074
karim064 <102268088+karim064@users.noreply.github.com>
2023-01-08 12:15:14 +0700

    edit
    biasalah

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Service />
+      <br />
       <Portofolio />
     </div>
   );
--- src/component/Service.js
+++ src/component/Service.js
@@ -3,31 +3,37 @@ import Card from 'react-bootstrap/Card';
 import web from '../assets/img/web-developer.jpg'
 import uiux from '../assets/img/uiux.jpg'
 import CardGroup from 'react-bootstrap/CardGroup';
+import '../App.css'
 
 function Service() {
   return (
-    <CardGroup style={{ width: '30rem' }}>
-        <Card >
-        <Card.Img variant="top" src={web} />
-        <Card.Body>
-            <Card.Title>Web Developer </Card.Title>
-            <Card.Text>
-            Website taek a
-            </Card.Text>
-            <Button variant="primary">Go</Button>
-        </Card.Body>
-        </Card>
-        <Card>
-        <Card.Img variant="top" src={uiux} />
-        <Card.Body>
-            <Card.Title>UI/UX Desaigner</Card.Title>
-            <Card.Text>
-            Desaign karepmu wes
-            </Card.Text>
-            <Button variant="primary">Go</Button>
-        </Card.Body>
-        </Card>
-    </CardGroup>
+    <div>
+      <h1>Jasa Service</h1>
+        <div className="list-service">
+        <CardGroup style={{ width: '30rem' }} >
+            <Card >
+            <Card.Img variant="top" src={web} />
+            <Card.Body>
+                <Card.Title>Web Developer </Card.Title>
+                <Card.Text>
+                Website taek a
+                </Card.Text>
+                <Button variant="primary">Go</Button>
+            </Card.Body>
+            </Card>
+            <Card>
+            <Card.Img variant="top" src={uiux} />
+            <Card.Body>
+                <Card.Title>UI/UX Desaigner</Card.Title>
+                <Card.Text>
+                Desaign karepmu wes
+                </Card.Text>
+                <Button variant="primary">Go</Button>
+            </Card.Body>
+            </Card>
+        </CardGroup>
+        </div>
+    </div>
   );
 }
 
