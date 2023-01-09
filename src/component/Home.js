fde6f90e3d601823eb92582639784f86dd9c840f
senja007 <aswinaditya007@gmail.com>
2023-01-09 10:16:21 +0700

    new file:   src/assets/img/pngwing.png 	modified:   src/component/Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,16 +1,69 @@
-import React from 'react';
-import '../App.css';
-import homebg from '../assets/img/home-bg.png';
+import React from 'react'
+import HeroImg from '../assets/img/pngwing.png'
+import Fade from 'react-reveal/Fade';
+import '../App.css'
 
-
-
-const Home = () => {
+export default function Hero() {
     return (
-            <div className='shapedividers_com-2787'>
-            <h1 br>ini adalah home</h1>
-            <img src={homebg} width={1500}/>
+        <section className="pt-0 hero">
+            <div className="wave-container">
+                <div className="container">
+                
+                <div className="row align-item-center" style={{ width: "360" }}>
+                    <Fade right delay={300}>
+                        <div className="col-auto pr-5">
+                            <h1 className="font-weight-bold my-5">
+                                NEON CODE TEAM
+                        </h1>
+                            <p className="mb-5 text-hero font-weight-light text-gray w-80 mt-3">
+                                Memberikan Pelayan Memuaskan <br />
+                                Silakan buat Website anda di sini <br />
+                                dengan gaya dan mempesona
+                            </p>
+                            <button className="btn btn-shop btn-primary" >
+                                Shop Now
+                        </button>
+                            <div className="row  icons-hero mt-100">
+                                <div className="col-auto icons">
+                                    <i className="far fa-paper-plane icon"></i>
+                                    <h6 className="mt-3">
+                                        Best Price <br />
+                                        <span className="text-gray-500 font-weight-light">
+                                            Guaranteed
+                                    </span>
+                                    </h6>
+                                </div>
+                                <div className="col-auto icons">
+                                    <i className="fas fa-tag"></i>
+                                    <h6 className="mt-3">
+                                        100% Secure <br />
+                                        <span className="text-gray-500 font-weight-light">
+                                            Payment online
+                                    </span>
+                                    </h6>
+                                </div>
+                                <div className="col-auto icons">
+                                    <i className="fas fa-undo-alt"></i>
+                                    <h6 className="mt-3">
+                                        Free revision <br />
+                                        <span className="text-gray-500 font-weight-light">
+                                            Within 2x
+                                    </span>
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </Fade>
+                    <div className="col-1 offset-md-2 pl-0 hero-img">
+                        <Fade bottom delay={350}>
+                            <div >
+                                <img src={HeroImg} alt="my-hero" height={540}/>
+                            </div>
+                        </Fade>
+                    </div>
+                </div>
+                </div>
             </div>
+        </section>
     )
 }
-
-export default Home;
