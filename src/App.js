a845f1d3ca6e0c796c411afd008e3dab43709b6c
dioproject <91179318+dioproject@users.noreply.github.com>
2023-01-09 15:03:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import NavBar from "./component/NavBar";
 import Home from "./component/Home";
 import Service from "./component/Service"
 import Portofolio from "./component/Portofolio";
-
 import Team from "../src/component/Team";
-import Home from "./component/Home";
 import Contact from "./component/Contact";
-
 
 function App() {
   return (
