3a23d0ae530e9db8ecceca3244e8863f35ee393c
dioproject <91179318+dioproject@users.noreply.github.com>
2023-01-09 15:03:05 +0700

    Update TeamCard.js

--- src/component/TeamCard.js
+++ src/component/TeamCard.js
@@ -3,9 +3,9 @@ import { Col } from "react-bootstrap";
 export const TeamCard = ({ name, skill, pap }) => {
     return(
         <Col size={12} sm={6} md={4}>
-            <div className="-imgbx">
+            <div className="team-imgbx">
                 <img src={pap} alt="img"/>
-                <div className="proj-txtx">
+                <div className="team-txtx">
                     <h4>{name}</h4>
                     <span>{skill}</span>
                 </div>
