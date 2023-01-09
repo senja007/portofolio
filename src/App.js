7f0cd7583b73d960489b79c3d336571d501f7272
senja007 <aswinaditya007@gmail.com>
2023-01-09 10:15:53 +0700

    jadi home

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import NavBar from "./component/NavBar";
 import Service from "./component/Service"
 import './App.css';
 import Portofolio from "./component/Portofolio";
+import Home from "./component/Home";
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
       <Service />
-      <br />
       <Portofolio />
     </div>
   );
---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import homebg from '../assets/img/home-bg.png';
+
+
+
+const Home = () => {
+    return (
+            <div className='shapedividers_com-2787'>
+            <h1 br>ini adalah home</h1>
+            <img src={homebg} width={1500}/>
+            </div>
+    )
+}
+
+export default Home;
