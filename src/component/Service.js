b09d72f9cf8fbcf55260144193b35a5c536f5c18
dioproject <91179318+dioproject@users.noreply.github.com>
2023-01-09 15:03:09 +0700

    Update Service.js

--- src/component/Service.js
+++ src/component/Service.js
@@ -5,34 +5,36 @@ import '../App.css'
 
 function Service() {
   return (
-    <div>
-      <h1>Layanan Kami</h1>
-      <h6>services we provide include</h6>
-      <div className='list-service'>
-        <CardGroup style={{ width: '30rem' }}>
-            <Card >
-            <Card.Img variant="top" src={web} />
-            <Card.Body>
-                <Card.Title>Web Developer</Card.Title>
-                  <Card.Text>
-                    Website taek a
-                  </Card.Text>
-                <Button variant="primary">Go</Button>
-            </Card.Body>
-            </Card>
-            <Card>
-            <Card.Img variant="top" src={uiux} />
-            <Card.Body>
-                <Card.Title>UI/UX Desaigner</Card.Title>
-                  <Card.Text>
-                    Desaign karepmu wes
-                  </Card.Text>
-                <Button variant="primary">Go</Button>
-            </Card.Body>
-            </Card>
-        </CardGroup>
+    <section className="service" id="service">
+      <div>
+        <h2>Layanan Kami</h2>
+        <p>services we provide include</p>
+        <div className='list-service'>
+          <CardGroup style={{ width: '30rem' }}>
+              <Card >
+              <Card.Img variant="top" src={web} />
+              <Card.Body>
+                  <Card.Title>Web Developer</Card.Title>
+                    <Card.Text>
+                      Website taek a
+                    </Card.Text>
+                  <Button variant="primary">Go</Button>
+              </Card.Body>
+              </Card>
+              <Card>
+              <Card.Img variant="top" src={uiux} />
+              <Card.Body>
+                  <Card.Title>UI/UX Desaigner</Card.Title>
+                    <Card.Text>
+                      Desaign karepmu wes
+                    </Card.Text>
+                  <Button variant="primary">Go</Button>
+              </Card.Body>
+              </Card>
+          </CardGroup>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
